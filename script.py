222fd993afdf500532a9b5bf2e9176dd30a740be
Nibesh Ranjit <nibesh_ranjit111@hotmail.com>
2026-09-01 15:43:36 -0400

    python 1

---
+++ script.py
@@ -0,0 +1,64 @@
+#variables
+x= 5
+y =('john')
+print (x)
+print (y)
+
+print ('*')
+#Casting
+x=str(3)
+y=int(3)
+z=float(3.3)
+
+x=5
+y="john"
+print(type(x))
+print(type(y))
+
+print('*')
+
+x = "John"
+# is the same as
+x = 'John'
+print(x)
+print('*')
+a = 4
+A = "Sally"
+#A will not overwrite a
+print(a, A)
+print('*')
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+print('*')
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+print('*')
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+print('*')
+x = "Python is awesome"
+print(x)
+print('*')
+x = "Python"
+y = "is"
+z = "awesome"
+a=234243
+print(x, y, z,a)
+print('*')
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+print('*')
+
+
+
